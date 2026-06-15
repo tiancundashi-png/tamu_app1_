40bb240b4882f445b5d05b27a0389fa22f3739c4
tatsuya <tiancundashi@gmail.com>
2026-06-15 17:30:28 +0900

    500ページの追加

--- shift-app/app.py
+++ shift-app/app.py
@@ -813,6 +813,11 @@ def unconfirm_shift(id):
 def page_not_found(error):
     return render_template("404.html"), 404
 
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"), 500
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
